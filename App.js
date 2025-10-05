0739148cad3599be537208d578c7f558d8d056f7
Pedro Coltro 👾 <138863769+coltrox@users.noreply.github.com>
2025-10-05 10:10:45 -0300

    att

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, 'useState', 'useEffect' from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
